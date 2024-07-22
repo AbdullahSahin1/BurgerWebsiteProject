c998feb51c1029a4fa76cdf41539d041ed508568
Abdullah Şahin <99759896+AbdullahSahin1@users.noreply.github.com>
2024-07-22 16:49:32 +0300

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,1 +1,21 @@
+import React from 'react'
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import XIcon from '@mui/icons-material/X';
+import '../Styles/Footer.css'
+const Footer = () => {
+  return (
+    <div className='footer'>
+        <div className='socialMedia'>
+            <FacebookIcon/>
+            <InstagramIcon/>
+            <XIcon/>
+        </div>
+        <p>
+            Tüm Hakları Saklıdır | Burger 
+        </p>
+    </div>
+  )
+}
 
+export default Footer
