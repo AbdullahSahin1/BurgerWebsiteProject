f6599b09797790e021de29b46b8f36d252feec8a
Abdullah Şahin <99759896+AbdullahSahin1@users.noreply.github.com>
2024-07-23 17:42:28 +0300

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,17 @@
 import React from 'react';
-
+import banneryeni from '../assets/banneryeni.jpg'
+import '../styles/about.css'
 export const About = () => {
-  return <div>About</div>;
+  return <div>
+    <div className="about">
+      <div className='aboutTop' style={{backgroundImage:`url(${banneryeni})`}}></div>
+      <div className='aboutBottom'>
+        <h1>Hakkımızda</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam temporibus atque quidem cum molestias itaque! Repudiandae architecto ratione minima quos, non ipsam similique cum quaerat doloremque aut pariatur saepe nostrum.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione sequi quisquam commodi saepe fugiat placeat explicabo adipisci possimus inventore, deserunt ipsum nostrum tempora assumenda consectetur distinctio! Optio esse ut fuga?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo beatae aspernatur nostrum ad iusto blanditiis vitae facere quis deserunt assumenda? Corrupti hic quia sequi voluptatum ea non et expedita nihil?
+        </p>
+      </div>
+    </div>
+  </div>;
 };
